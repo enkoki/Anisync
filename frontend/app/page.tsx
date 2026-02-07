257e6acdfa834c3fd612e1ada5ee5dd9605cfe04
Rijan Dahal <reasondahal719@gmail.com>
2026-02-07 23:10:50 +0545

    Feat: add Login route and Navbar component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
+  const isLoggedIn = false; // toggle this value to switch from logged out to log in and vice versa
   return (
-    <p>Hello World</p>
+    <>
+      <Navbar isloggedin={isLoggedIn}/>
+    </>
   );
 }
