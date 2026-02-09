55d91537a467dda186122fde5ce4318cfa9d8f02
Rijan Dahal <reasondahal719@gmail.com>
2026-02-09 14:25:30 +0545

    Fix:navbar responsiveness

---
+++ frontend/app/assets/icons/Hamburger.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hamburger = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" fill="white" className="bi bi-list xl:hidden" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+    </svg>
+  )
+}
+
+export default Hamburger
--- frontend/app/components/Navbar/Navbar.tsx
+++ frontend/app/components/Navbar/Navbar.tsx
@@ -8,21 +8,23 @@ import SiteLogoL from '@/app/assets/images/anisync_light.png'
 import SiteLogoD from '@/app/assets/images/anisync_dark.png'
 import Variant from '../Button/Variant';
 import LoggedIn from '../LoggedIn/LoggedIn';
+import Hamburger from '@/app/assets/icons/Hamburger';
 const Navbar = ({isloggedin = false}) => {
 
     const pathname = usePathname();
 
     return (
-    <header className='min-w-screen h-[75px] py-2 px-25 flex justify-between items-center bg-[#040404] shadow-2xl fixed z-50'>
+    <header className='w-screen h-[75px] py-2 px-10 xl:px-25 flex justify-between items-center bg-[#040404] shadow-2xl fixed z-50'>
         <Image src={SiteLogoD} alt="anisync-logo" className='w-13 h-13'/>
-        <nav>
+        <nav className='hidden xl:flex'>
             <ul className='flex gap-10 font-bold text-[20px] justify-center items-center'>
                 <li className={pathname === '/about' ? 'text-[#6200ED]' : ''}><Link href="/about">About</Link></li>
                 <li className={pathname === '/profile' ? 'text-[#6200ED]' : ''}><Link href="/profile">Profile</Link></li>
                 <li className={pathname === '/' ? 'text-[#6200ED]' : ''}><Link href="/">Home</Link></li>
             </ul>
         </nav>
-        {isloggedin ? <LoggedIn/> : <Variant content="Log In"/>}
+        {isloggedin ? <div className='hidden xl:flex'><LoggedIn/></div> : <Variant content="Log In"/>}
+        <Hamburger/>
     </header>
     );
 }
