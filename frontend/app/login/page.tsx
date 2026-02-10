31071e6b9aba2f60edeeb50cca1bd9a0b4214b22
yepMizu <luyashthapa123@gmail.com>
2026-02-10 21:21:11 +0545

    Feat: Add Log-In Page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,13 +1,16 @@
 
-import Box from "../components/LoginBox/box" 
+import Box from "../components/LoginBox/box"
 import Image from "next/image"
-import picture from "./image"
+import Picture from "../assets/icons/img"
+
 
 export default function LoginPage() {
     return (
-        <div className="flex">
-            <Box/>
-            <picture/>
+        <div className="flex bg-black justify-center min-h-screen items-center  ">
+            <div className="w-[1500px] h-[950px] flex justify-center items-center">
+                <Picture />
+                <Box />
+            </div>
         </div>
     )
 }
