e1e495dbef3a230c1c3e3b5bab0468690a831156
Rijan Dahal <reasondahal719@gmail.com>
2026-02-12 20:38:51 +0545

    Feat: add JWT Authentication

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -1,9 +1,12 @@
 from fastapi import HTTPException, status, Depends
+from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 
+from db import get_db
+from schemas.token_schemas import TokenResponse
 from config import SECRET_KEY, ALGORITHM, EXPIRATION_TIME
 from models import Users
 
@@ -12,6 +15,9 @@ hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return hasher.hash(password)
+
+def verify_password(password: str, hashed_password: str):
+    return hasher.verify(password, hashed_password)
 
 def register_user(user, db):
     if db.query(Users).filter(Users.username == user.username).first():
@@ -27,16 +33,43 @@ def register_user(user, db):
     db.refresh(new_user)
     return new_user
 
-def login():
-    pass
+def login_user(user_data, db):
+    user = db.query(Users).filter(Users.username == user_data.username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
+    if not verify_password(user_data, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
+    
+    access_token = create_access_token(data = {"sub":user.username})
+    return {"access_token":access_token, "token_type": "bearer"}
 
-def create_access_token():
-    pass
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expires_at = datetime.utcnow() + timedelta(minutes=EXPIRATION_TIME)
 
-def verify_token():
-    pass
+    #adding expiration date to payload
+    to_encode.update({"exp":expires_at})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str) -> TokenResponse:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not verify credentials",headers={"WWW-Authenticate":"Bearer"})
+        
+        return TokenResponse(username = username)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not verify credentials",headers={"WWW-Authenticate":"Bearer"})
 
 
-def get_current_user():
-    pass
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    token_data = verify_token(token)
+    user = db.query(Users).filter(Users.username==token_data.username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="User does not exist",headers={"WWW-Authenticate":"Bearer"})
+    return user
     
