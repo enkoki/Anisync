9a65b07b653b1fee957e744a93941779fa6292f9
Rijan Dahal <reasondahal719@gmail.com>
2026-02-12 20:59:16 +0545

    Fix: Add CORSMiddlewares & final backend fixes
    Co-authored-by: yepMizu <luyashthapa123@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-from db import engine, SessionLocal, Base, get_db
-from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
+from db import engine, Base
 from routers import api_router
-from config import DATABASE_URL
 import models 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"] # "https://yourfrontend.com"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
