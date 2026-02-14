d8504a747cd3aba8098cb62a772edc93b8bbf648
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-02-14 15:46:01 +0000

    WIP: icons in about page

---
+++ frontend/app/components/AboutIconsFeatures/AboutIcons.jsx
@@ -0,0 +1,9 @@
+function AboutIcon({ source }) {
+  return (
+    <div>
+      {/* className="h-50 w-50 bg-cover bg-center bg-no-repeat" */}
+      {/* style={{ backgroundImage: `url(${source})` }} */}
+    </div>
+  );
+}
+export default AboutIcon;
---
+++ frontend/app/components/AboutIconsFeatures/AboutIconsFeatures.jsx
@@ -0,0 +1,22 @@
+// import AboutIcon from "./AboutIcons.jsx";
+// import Icon1 from "../../assets/icons/custom-1.svg";
+// import Icon2 from "../../assets/icons/custom-1.svg";
+// import Icon3 from "../../assets/icons/socail-1.svg";
+// import Icon4 from "../../assets/icons/custom-1.svg";
+
+function AboutIconsFeatures() {
+  return (
+    <div className="flex flex-col items-center text-white w-screen h-500">
+      <div className="">
+        WHY
+        <span className="">ANYSYNC</span>
+      </div>
+      <div className="grid grid-col-2 grid-row-2">
+        {/* <AboutIcon source={Icon1} /> */}
+        {/* <AboutIcon source="../../assets/icons/social-1.svg" /> */}
+        {/* <AboutIcon source="../../assets/icons/custom-1.svg" /> */}
+      </div>
+    </div>
+  );
+}
+export default AboutIconsFeatures;
