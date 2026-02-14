92285937af475a754a6974b45c2bb152566008d4
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-02-14 16:25:22 +0000

    Feat: add svg 's'

--- frontend/app/components/AboutIconsFeatures/AboutIcons.jsx
+++ frontend/app/components/AboutIconsFeatures/AboutIcons.jsx
@@ -1,9 +1,9 @@
-function AboutIcon() {
+function AboutIcon({ source }) {
   return (
-    <div>
-      {/* className="h-50 w-50 bg-cover bg-center bg-no-repeat" */}
-      {/* style={{ backgroundImage: `url(${source})` }} */}
-    </div>
+    <div
+      className="h-[200px] w-[200px] bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${source})` }}
+    ></div>
   );
 }
 export default AboutIcon;
--- frontend/app/components/AboutIconsFeatures/AboutIconsFeatures.jsx
+++ frontend/app/components/AboutIconsFeatures/AboutIconsFeatures.jsx
@@ -1,20 +1,23 @@
-// import AboutIcon from "./AboutIcons.jsx";
-// import Icon1 from "../../assets/icons/custom-1.svg";
-// import Icon2 from "../../assets/icons/custom-1.svg";
-// import Icon3 from "../../assets/icons/socail-1.svg";
-// import Icon4 from "../../assets/icons/custom-1.svg";
+import AboutIcon from "./AboutIcons.jsx";
+import Icon1 from "../../assets/icons/stats-1.svg";
+import Icon2 from "../../assets/icons/apps-1.svg";
+import Icon3 from "../../assets/icons/social-1.svg";
+import Icon4 from "../../assets/icons/custom-1.svg";
 
 function AboutIconsFeatures() {
   return (
-    <div className="flex flex-col items-center text-white w-screen h-500">
-      <div className="">
+    <div className="flex flex-col items-center text-white w-screen h-200 p-30">
+      <div className="w-[500px] h-[100px] text-[64px] font-black">
         WHY
-        <span className="">ANYSYNC</span>
+        <span className="bg-[#6A00FF] rounded-[15px] m-[16px] h-[100px] p-3">
+          ANYSYNC
+        </span>
       </div>
-      <div className="grid grid-col-2 grid-row-2">
-        {/* <AboutIcon source={Icon1} /> */}
-        {/* <AboutIcon source="../../assets/icons/social-1.svg" /> */}
-        {/* <AboutIcon source="../../assets/icons/custom-1.svg" /> */}
+      <div className="grid grid-cols-2 grid-rows-2 w-screen h-[500px] place-items-center gap-40">
+        <AboutIcon source={Icon1.src} />
+        <AboutIcon source={Icon2.src} />
+        <AboutIcon source={Icon3.src} />
+        <AboutIcon source={Icon4.src} />
       </div>
     </div>
   );
