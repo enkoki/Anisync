c82a0cc0cbbb6a51b1e1e6c73a704dcb0f19a1f3
Rijan Dahal <reasondahal719@gmail.com>
2026-02-16 10:40:10 +0545

    Feat: Get User Endpoint

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import Session
 
 from db import get_db
 from schemas.user_schemas import UserCreate, UserLogin, UserOut
-from services.auth import register_user, login_user
+from services.auth import register_user, login_user, get_current_user
 
 public_router = APIRouter(prefix = "/auth", tags=["Users"])
-router = APIRouter(tags=["Users"])
+router = APIRouter(tags=["Users"], dependencies=[Depends(get_current_user)])
 
 @public_router.post("/register", response_model=UserOut)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -16,3 +16,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 @public_router.post("/login")
 def login_for_token(user_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     return login_user(user_data, db)
+
+@router.get("/users", response_model=UserOut)
+def get_user(current_user: UserOut = Depends(get_current_user)):
+    return current_user
