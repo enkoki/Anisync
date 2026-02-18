1587bc8675cdde5d8be5892c952ead04157649dd
yepMizu <luyashthapa123@gmail.com>
2026-02-18 23:00:24 +0545

    Feat: Add image side of register box... Left to make it good

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,10 +1,13 @@
 import RegisterPage from "../components/RegisterBox/box"
+import Picture from "../assets/icons/img"
 
 export default function RegisterBox(){
     return(
        <div className="flex bg-black justify-center min-h-screen items-center">
             <div className="w-375 h-237.5 flex justify-center items-center">
-                <RegisterPage/>
+                <Picture>
+                    <RegisterPage/>
+                </Picture>
             </div>
         </div>
     )
