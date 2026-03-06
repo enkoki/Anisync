fbec0516cfd4d3124ca29ed26b9e7bfd954347bb
Rijan Dahal <reasondahal719@gmail.com>
2026-03-07 01:51:26 +0545

    Update: Sidebar Design and user interaction

--- frontend/app/components/Dashboard/components/DBSidebar/SideItems.tsx
+++ frontend/app/components/Dashboard/components/DBSidebar/SideItems.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import HomeIcon from "@/app/assets/icons/sidebar_icons/Home";
 import ConnectionIcon from "@/app/assets/icons/sidebar_icons/Connections";
 import SettingsIcon from "@/app/assets/icons/sidebar_icons/Settings";
+import ShieldIcon from "@/app/assets/icons/sidebar_icons/Shield";
 
 export interface SidebarItemType {
     id: string;
@@ -12,5 +13,6 @@ export interface SidebarItemType {
 export const sidebarItems: SidebarItemType[] = [
     { id: "overview", label: "Overview", icon: <HomeIcon /> }, 
     { id: "users", label: "Users", icon: <ConnectionIcon/> },
+    { id: "admins", label: "Admins", icon: <ShieldIcon /> },
     { id: "settings", label: "Settings", icon: <SettingsIcon />}
 ];
